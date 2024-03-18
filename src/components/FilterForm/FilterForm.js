8ee11c62db4e20006090bc8185aae2c97b27998f
Martozhyna <mybonanzaluck@gmail.com>
2024-03-18 18:01:06 +0200

    changed data input

--- src/components/FilterForm/FilterForm.js
+++ src/components/FilterForm/FilterForm.js
@@ -1,88 +1,88 @@
-import {useDispatch, useSelector} from "react-redux";
-import {useEffect} from "react";
+    import {useDispatch, useSelector} from "react-redux";
+    import {useEffect} from "react";
 
-import {ModalFormInput} from "../ModalFormInput/ModalFormInput";
-import css from './FilterForm.module.css';
-import {ModalFormWithChoice} from "../ModalFormWithChoice/ModalFormWithChoice";
-import {groupAction, orderActions} from "../../redux";
+    import {ModalFormInput} from "../ModalFormInput/ModalFormInput";
+    import css from './FilterForm.module.css';
+    import {ModalFormWithChoice} from "../ModalFormWithChoice/ModalFormWithChoice";
+    import {groupAction, orderActions} from "../../redux";
 
-const FilterForm = ({search, setSearch, register, handleSubmit}) => {
+    const FilterForm = ({search, setSearch, register, handleSubmit}) => {
 
-    const { groups } = useSelector((state) => state.group);
-    const {page} = useSelector(state => state.order)
-    const dispatch = useDispatch();
+        const { groups } = useSelector((state) => state.group);
+        const {page} = useSelector(state => state.order)
+        const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(groupAction.getAll());
-    }, [dispatch])
+        useEffect(() => {
+            dispatch(groupAction.getAll());
+        }, [dispatch])
 
-    useEffect(() => {
-        if (search.size > 1){
-            dispatch(orderActions.getAll(search))
+        useEffect(() => {
+            if (search.size > 1){
+                dispatch(orderActions.getAll(search))
+            }
+
+        }, [dispatch, search])
+
+        const submit = async (data) => {
+            const cleanedData = Object.fromEntries(
+                Object.entries(data).filter(([key, value]) => value !== "")
+            );
+            await Object.entries(cleanedData).forEach(([key, value]) => {
+                setSearch((currentQuery) => {
+                    const existingValues = currentQuery.getAll(key) || [];
+                    const newValues = Array.isArray(value) ? value : [value];
+                    currentQuery.set(key, newValues);
+                    currentQuery.set("page", page);
+                    return currentQuery;
+                });
+            })
         }
 
-    }, [dispatch, search])
+        return (
+            <form onChange={handleSubmit(submit)}>
+                <div className={css.first}>
+                    <div>
+                        <ModalFormInput type={'text'} name={'name'} label={'name'} addLabel={false} register={register}/>
+                        <ModalFormInput type={'text'} name={'surname'} label={'surname'} addLabel={false} register={register}/>
+                    </div>
+                    <div>
+                        <ModalFormInput type={'text'} name={'email'} label={'email'} addLabel={false} register={register}/>
+                        <ModalFormInput type={'text'} name={'phone'} label={'phone'} addLabel={false} register={register}/>
+                    </div>
+                    <div>
+                            <ModalFormInput type={'text'} name={'age'} label={'age'} addLabel={false} register={register}/>
+                            <ModalFormWithChoice name={'course'} label={'course'} addLabel={false} register={register}
+                                                 options={['FS', 'QACX', 'JCX', 'JSCX', 'FE', 'PCX']}
+                                                 defaultLabel={'course'}/>
+                    </div>
+                    <div>
+                            <ModalFormWithChoice name={'course_format'} label={'course format'} addLabel={false}
+                                                 register={register} options={['static', 'online']}
+                                                 defaultLabel={'course format'}/>
+                            <ModalFormWithChoice name={'course_type'} label={'course type'} addLabel={false}
+                                                 register={register}
+                                                 options={['pro', 'minimal', 'premium', 'incubator', 'vip']}
+                                                 defaultLabel={'course type'}/>
+                    </div>
+                    <div>
+                            <ModalFormWithChoice name={'status'} label={'status'} addLabel={false} register={register}
+                                                 options={['In work', 'New', 'Agree', 'Disagree', 'Dubbing']}
+                                                 defaultLabel={'status'}/>
+                            <ModalFormWithChoice name={'group'} label={'group'} addLabel={false} register={register}
+                                                 defaultLabel={'group'}
+                                                 options={groups && groups.map((group) => ({
+                                                     value: group.title,
+                                                     label: group.title
+                                                 }))}/>
+                    </div>
+                    <div>
+                        <ModalFormInput type={'date'} name={'start_date'} label={'start_date'} addLabel={false} register={register} placeholder={'start data'}/>
+                        <ModalFormInput type={'date'} name={'end_date'} label={'end_date'} addLabel={false} register={register} placeholder={'end data'}/>
+                    </div>
 
-    const submit = async (data) => {
-        const cleanedData = Object.fromEntries(
-            Object.entries(data).filter(([key, value]) => value !== "")
-        );
-        await Object.entries(cleanedData).forEach(([key, value]) => {
-            setSearch((currentQuery) => {
-                const existingValues = currentQuery.getAll(key) || [];
-                const newValues = Array.isArray(value) ? value : [value];
-                currentQuery.set(key, newValues);
-                currentQuery.set("page", page);
-                return currentQuery;
-            });
-        })
+                </div>
+
+            </form>
+        )
     }
-
-    return (
-        <form onChange={handleSubmit(submit)}>
-            <div className={css.first}>
-                <div>
-                    <ModalFormInput type={'text'} name={'name'} label={'name'} addLabel={false} register={register}/>
-                    <ModalFormInput type={'text'} name={'surname'} label={'surname'} addLabel={false} register={register}/>
-                </div>
-                <div>
-                    <ModalFormInput type={'text'} name={'email'} label={'email'} addLabel={false} register={register}/>
-                    <ModalFormInput type={'text'} name={'phone'} label={'phone'} addLabel={false} register={register}/>
-                </div>
-                <div>
-                        <ModalFormInput type={'text'} name={'age'} label={'age'} addLabel={false} register={register}/>
-                        <ModalFormWithChoice name={'course'} label={'course'} addLabel={false} register={register}
-                                             options={['FS', 'QACX', 'JCX', 'JSCX', 'FE', 'PCX']}
-                                             defaultLabel={'course'}/>
-                </div>
-                <div>
-                        <ModalFormWithChoice name={'course_format'} label={'course format'} addLabel={false}
-                                             register={register} options={['static', 'online']}
-                                             defaultLabel={'course format'}/>
-                        <ModalFormWithChoice name={'course_type'} label={'course type'} addLabel={false}
-                                             register={register}
-                                             options={['pro', 'minimal', 'premium', 'incubator', 'vip']}
-                                             defaultLabel={'course type'}/>
-                </div>
-                <div>
-                        <ModalFormWithChoice name={'status'} label={'status'} addLabel={false} register={register}
-                                             options={['In work', 'New', 'Agree', 'Disagree', 'Dubbing']}
-                                             defaultLabel={'status'}/>
-                        <ModalFormWithChoice name={'group'} label={'group'} addLabel={false} register={register}
-                                             defaultLabel={'group'}
-                                             options={groups && groups.map((group) => ({
-                                                 value: group.title,
-                                                 label: group.title
-                                             }))}/>
-                </div>
-                <div>
-                    <ModalFormInput type={'date'} name={'start_date'} label={'start_date'} addLabel={false} register={register}/>
-                    <ModalFormInput type={'date'} name={'end_date'} label={'end_date'} addLabel={false} register={register}/>
-                </div>
-
-            </div>
-
-        </form>
-    )
-}
-export {FilterForm}
+    export {FilterForm}
--- src/components/ModalFormInput/ModalFormInput.js
+++ src/components/ModalFormInput/ModalFormInput.js
@@ -1,12 +1,14 @@
 import css from './ModalFormInput.module.css';
 
-const ModalFormInput = ({ register, label, id, name, addLabel = true, ...inputProps }) => {
+const ModalFormInput = ({ register, label, id, name, addLabel = true, placeholder, ...inputProps }) => {
+
+
     return (
         <div className={css.container}>
             {addLabel ? <label >{label}</label> : null}
 
             <input
-                placeholder={label}
+                placeholder={placeholder || label}
                 {...register(name)}
                 {...inputProps}
                 className={css.input}
