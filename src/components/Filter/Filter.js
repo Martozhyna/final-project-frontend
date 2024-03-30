45a5c14474ec5143a7a7a1e3146d4822ba93680b
Martozhyna <mybonanzaluck@gmail.com>
2024-03-30 16:12:13 +0200

    fixed url my button error

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -3,10 +3,12 @@ import {useForm} from "react-hook-form";
 import {FilterForm} from "../FilterForm/FilterForm";
 import {FilterOption} from "../FilterOption/FilterOption";
 import css from './Filter.module.css';
+import {useState} from "react";
 
 const Filter = ({search, setSearch}) => {
 
     const {register, handleSubmit, reset, setValue} = useForm({mode:"all"})
+    const [showMyOrders, setShowMyOrders] = useState(false);
 
     const handleReset = () => {
         setValue("name", "");
@@ -21,6 +23,7 @@ const Filter = ({search, setSearch}) => {
         setValue("group", "");
         setValue("start_date", "");
         setValue("end_date", "");
+        setShowMyOrders(false)
 
         const params = new URLSearchParams();
         params.set("page", "1");
@@ -33,7 +36,7 @@ const Filter = ({search, setSearch}) => {
     return (
         <div className={css.main}>
             <FilterForm search={search} setSearch={setSearch} register={register} handleSubmit={handleSubmit} setValue={setValue}/>
-            <FilterOption setSearch={setSearch} reset={reset} handleReset={handleReset}/>
+            <FilterOption setSearch={setSearch} reset={reset} handleReset={handleReset} setShowMyOrders={setShowMyOrders} showMyOrders={showMyOrders}/>
         </div>
     )
 }
--- src/components/FilterOption/FilterOption.js
+++ src/components/FilterOption/FilterOption.js
@@ -1,18 +1,21 @@
-import {useDispatch, useSelector} from "react-redux";
-import {useEffect, useState} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect} from "react";
 
 import css from './FilterOption.module.css';
-import {userActions} from "../../redux";
+import { userActions } from "../../redux";
 
-const FilterOption = ({setSearch, reset, handleReset}) => {
-
+const FilterOption = ({ setSearch, reset, handleReset, setShowMyOrders, showMyOrders }) => {
     const { user } = useSelector((state) => state.user);
-    const [showMyOrders, setShowMyOrders] = useState(false);
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(userActions.getMe())
     }, [dispatch])
+
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+        setShowMyOrders(params.get('manager') === user.surname);
+    }, [user.surname, setShowMyOrders]);
 
     const click = () => {
         setSearch((currentQuery) => {
@@ -25,16 +28,15 @@ const FilterOption = ({setSearch, reset, handleReset}) => {
             return query;
         });
         setShowMyOrders(!showMyOrders);
-
     }
-
 
     return (
         <div>
             <button className={showMyOrders ? `${css.btn} ${css.active}` : css.btn} onClick={click}>My</button>
             <button className={css.btn} onClick={handleReset}>Reset</button>
-            <button className={css.btn}>Exel</button>
+            <button className={css.btn}>Excel</button>
         </div>
-    )
+    );
 }
-export {FilterOption}
+
+export { FilterOption };
