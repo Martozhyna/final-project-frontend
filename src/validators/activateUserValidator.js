51efa6a0d63a4318e8ea4bb3be82323f350b95d8
Martozhyna <mybonanzaluck@gmail.com>
2024-04-21 16:43:47 +0300

    added activate password validator

--- src/validators/activateUserValidator.js
+++ src/validators/activateUserValidator.js
@@ -1,8 +1,8 @@
 import * as yup from "yup"
 
 const activateUserValidator = yup.object({
-    password: yup.string().max(128, "Max 128 char"),
-    confirm: yup.string().oneOf([yup.ref('password'), null], 'Паролі мають співпадати').max(128, "Max 128 char")
+    password: yup.string().min(6, 'Мінімум 6 символів').matches(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s])+$/, 'Пароль має містити мінімум 1 цифру, 1 велику та маленьку букви та 1 спец. символ').max(12, "Максимум 12 символів"),
+    confirm: yup.string().oneOf([yup.ref('password'), null], 'Паролі мають співпадати').min(6, 'Мінімум 6 символів').matches(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s])+$/, 'Пароль має містити мінімум 1 цифру, 1 велику та маленьку букви та 1 спец. символ').max(12, "Максимум 12 символів"),
 })
 
 export {activateUserValidator}
